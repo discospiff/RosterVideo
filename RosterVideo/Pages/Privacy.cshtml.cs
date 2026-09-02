1f85524763b455f67029f260fb9dd2b02f8982b4
Jones, Brandan (jonesbr) <jonesbr@ucmail.uc.edu>
2026-09-01 20:07:16 -0400

    Add project files.

---
+++ RosterVideo/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RosterVideo.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
